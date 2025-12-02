6f529bfaa1fd48b6c14d265e5df1d6ebd1fc597f
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-02 14:20:21 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.Clear()
+        Console.CursorVisible = False
+        Console.CursorTop = 10
+        Console.CursorLeft = 10
+        Console.Write("Hello,World!!!")
+        Console.ReadKey()
+    End Sub
+End Module
