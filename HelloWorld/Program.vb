e28dbb1807b3525520ed220ba34ca02b2a9e1c6f
Alexander <pchp250g8@tutamail.com>
2026-03-10 19:52:47 +0300

    Update Program.vb

--- HelloWorld/Program.vb
+++ HelloWorld/Program.vb
@@ -4,9 +4,10 @@ Module Program
     Sub Main(args As String())
         Console.Clear()
         Console.CursorVisible = False
-        Console.CursorTop = 10
-        Console.CursorLeft = 10
+        Console.SetCursorPosition(10, 10)
         Console.Write("Hello,World!!!")
+        Console.SetCursorPosition(10, 11)
+        Console.Write("VB.Net DotNet Core Platform!!!")
         Console.ReadKey()
     End Sub
 End Module
